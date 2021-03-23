fd7437929a71e792aace257d09502c3f0dadf925
lucassilveira96 <lucas.silveira96@ulbra.edu.br>
2021-03-23 01:16:58 -0300

    delete

--- src/main/java/com/example/api/dao/TicketDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.api.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.api.models.Ticket;
-
-public interface TicketDao extends CrudRepository<Ticket, Long>{
-
-}
